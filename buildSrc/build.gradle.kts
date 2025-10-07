31e5eb4233ae2033fda91037d1cea79b6c0d3f10
querti <38067220+querti@users.noreply.github.com>
2025-10-07 13:57:46 +0200

    Update dependency org.apache.commons:commons-lang3 to v3.19.0
    Signed-off-by:

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -11,7 +11,7 @@ dependencies {
     // These dependencies have older versions that Renovate can update
     implementation("com.fasterxml.jackson.core:jackson-databind:2.12.7")
     implementation("org.slf4j:slf4j-api:1.7.36")
-    implementation("org.apache.commons:commons-lang3:3.12.0")
+    implementation("org.apache.commons:commons-lang3:3.19.0")
     
     // Gradle API
     implementation(gradleApi())
